048716cf4d89c217c7d14d7d6970e403b4b0fa1a
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2023-11-15 20:21:57 -0500

    Write two greeting methods

--- final_practice.rb
+++ final_practice.rb
@@ -5,16 +5,26 @@
 
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
+def greet_people 
+    "Hello everyone"
+end
+    puts greet_people
+# What is the return value of your method? "Hello everyone"
+# How many arguments did you pass your method? 0
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
+def custom_greeting(name)
+   "hello #{name}"
+end    
+odells_greeting = custom_greeting("Odell")
+luis_greeting = custom_greeting("Luis")
+puts odells_greeting
+puts luis_greeting
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? "hello #{name}" name is going to be any name 
+# How many arguments did you pass your method? 1
+# What data type was your argument(s)? string 
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
@@ -35,14 +45,14 @@
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
-check_stock(4, "Coffee");
-# => "Coffee is stocked"
+# check_stock(4, "Coffee");
+# # => "Coffee is stocked"
 
-check_stock(3, "Tortillas");
-# => "Tortillas - running LOW"
+# check_stock(3, "Tortillas");
+# # => "Tortillas - running LOW"
 
-check_stock(0, "Cheese");
-# => "Cheese - OUT of stock!"
+# check_stock(0, "Cheese");
+# # => "Cheese - OUT of stock!"
 
-check_stock(1, "Salsa");
-# => "Salsa - running LOW"
+# check_stock(1, "Salsa");
+# # => "Salsa - running LOW"
